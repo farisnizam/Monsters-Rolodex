e4c12ca0a620e17f8e2b424cbae40e95a1fd56d7
Farisnizam <farismtq@gmail.com>
2022-09-17 22:13:21 +0800

    convert class to functional component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,36 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.monster;
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
 
-    return (
-      <div className="card-container">
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+// class Card extends Component {
+//   render() {
+//     const { id, name, email } = this.props.monster;
+
+//     return (
+//       <div className="card-container">
+//         <img
+//           alt={`monster ${name}`}
+//           src={`https://robohash.org/${id}?set=set2&size=180x180`}
+//         />
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default Card;
