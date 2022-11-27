6274276a2e70ea41f49fa8ed9e5900b72047dc22
farisnizam <farismtq@gmail.com>
2022-11-27 12:39:35 +0800

    add type to card-list component

---
+++ src/components/card-list/card-list.component.tsx
@@ -0,0 +1,18 @@
+import Card from "../card/card.component";
+import { Monster } from "../../App";
+import "./card-list.styles.css";
+
+type CardListProps = {
+  monsters: Monster[];
+};
+
+const CardList = ({ monsters }: CardListProps) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      const { id } = monster;
+      return <Card key={id} monster={monster} />;
+    })}
+  </div>
+);
+
+export default CardList;
