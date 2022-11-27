4a14c41c0a757efd7523e1e1c882ba66f48f3b69
farisnizam <farismtq@gmail.com>
2022-11-27 11:58:59 +0800

    add typescript, add type to search-box component

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,24 @@
+// import { Component } from "react";
+import { ChangeEventHandler } from "react";
+import "./search-box.styles.css";
+
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+};
+
+const SearchBox = ({
+  onChangeHandler,
+  placeholder,
+  className,
+}: SearchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
